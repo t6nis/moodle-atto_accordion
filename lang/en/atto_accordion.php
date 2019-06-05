b11f255495d9021979f13c78f944621d9037926c
Tõnis Tartes <t6nis20@gmail.com>
2019-06-05 11:50:55 +0300

    Atto accordion plugin init

---
+++ lang/en/atto_accordion.php
@@ -0,0 +1,28 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'atto_accordion', language 'en'.
+ *
+ * @package    atto_accordion
+ * @copyright  2014 Tõnis Tartes <tonis.tartes@gmail.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['pluginname'] = 'Accordion';
+$string['createaccordion'] = 'Create accordion';
+$string['accordiontitle'] = 'Accordion title';
+$string['accordioncontent'] = 'Accordion content';
